d62d81450c76f6eaf81e901cf88d5510edb60b08
LevonPowell <levon.zadravec.powell@gmail.com>
2013-01-14 08:54:37 -0500

    Original Commit
    Taking my "aggressive" player and adding a tiny bit of junk. Also added
    a Sensory class. More to come!

---
+++ RobotPlayer.java
@@ -0,0 +1,186 @@
+package MasterPlayer;
+
+import battlecode.common.Clock;
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.GameConstants;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+
+/**
+ * This is my "master" player, as in the Master branch of a repository.
+ * 
+ * As of Monday January 14th, 2013, this is simply "aggressive player" with
+ * some of it's behaviour factored and abstracted in the Sensory class.
+ * 
+ * HQ Behaviours:
+ * 
+ * If the closest square to the enemy cannot spawn, pick another one randomly.
+ * 
+ * Senses nuke research now.
+ * 
+ * Solider Behaviours:
+ * 
+ * @author levonpowell
+ */
+public class RobotPlayer 
+{
+	private static final double MINE_LAYING_PROBABILITY = 0.01;
+	
+	private static int  ENEMY_DETECTION_RANGE = 15;
+	private static int  TEAM_DETECTION_RANGE = 7;
+	
+	/**
+	 * This is the only required method of this class. I will likely want to break a bunch of
+	 * this functionality into new classes.
+	 * 
+	 * @param rc is the base class which provides the majority of the functionality.
+	 */
+	public static void run( RobotController rc ) 
+	{
+		while ( true ) 
+		{
+			try 
+			{
+				if ( rc.getType() == RobotType.HQ ) 
+				{
+					HQActions( rc ); // This handles all HQ actions.
+				} 
+				else if ( rc.getType() == RobotType.SOLDIER )
+				{
+					soldierActions( rc );
+				}
+				// End turn
+				rc.yield(); // yield returns all extra bytecodes, and makes sure nothing is left over for next round.
+			} 
+			catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private static void HQActions( RobotController robotController ) throws GameActionException
+	{
+		if ( robotController.isActive() ) 
+		{
+			// This is the earliest round they can be half way done.
+			
+			// This also needs to not sweat it if it's a small map.
+			if ( Clock.getRoundNum() > 199 )
+			{
+				boolean enemyNukeResearch = robotController.senseEnemyNukeHalfDone();
+				
+				// Then do something I guess.
+			}
+			
+			// Spawn a soldier in the direction of the enemy camp
+			Direction dir = Sensory.getDirectionToEnemyHQ( robotController );
+
+			if ( robotController.canMove( dir ) )
+			{
+				robotController.spawn( dir );
+			}
+			else
+			{
+				Direction random = Direction.values()[ ( int )( Math.random()*8 ) ];
+				
+				if ( robotController.canMove( random ) )
+				{
+					robotController.spawn( random );
+				}
+			}
+		}
+	}
+	
+	private static void soldierActions( RobotController robotController ) throws GameActionException
+	{
+		if ( robotController.isActive() ) 
+		{
+			Direction dir; // Generic direction initialization
+			int allies, enemies;
+			
+			// Get a map of all the enemies. If none are nearby, lay some mines down.
+			
+			enemies = Sensory.getNumberOfLocalEnemies( robotController, ENEMY_DETECTION_RANGE );
+			allies = Sensory.getNumberOfLocalAllies( robotController, TEAM_DETECTION_RANGE );
+			
+			robotController.setIndicatorString( 1, "Enemy Soldiers in proximity: " + enemies );
+			robotController.setIndicatorString( 2, "Team Soldiers in proximity: " + allies );
+			
+			if ( Math.random() < MINE_LAYING_PROBABILITY && enemies < 2 ) 
+			{
+				// Lay a mine 
+				if( robotController.senseMine( robotController.getLocation() ) == null )
+				{
+					robotController.layMine();
+				}
+			} 
+			else
+			{
+				// Half of the time I want new units moving towards the enemy base anyways.
+				if ( enemies > 1 && allies <= 4 )
+				{
+					// Fall back until there is a numbers advantage
+					dir = Sensory.getDirectionToHQ( robotController );
+				}
+				// If the game is early, see if we move randomly.
+				else if ( Math.random() < 0.45 && Clock.getRoundNum() < 1000 )
+				{
+					dir = Direction.values()[ ( int )( Math.random()*8 ) ];
+				}
+				else
+				{
+					// If late game, move towards the enemy base.
+					if ( Clock.getRoundNum() >= 1000 )
+					{
+						if ( Math.random() < 0.025 )
+						{
+							dir = Direction.values()[ ( int )( Math.random()*8 ) ];
+						}
+						else
+						{
+							dir = Sensory.getDirectionToEnemyHQ( robotController );
+						}
+					}
+					else
+					{
+						// Default move towards enemy base code.
+						dir = Sensory.getDirectionToEnemyHQ( robotController );
+					}
+				}
+				// Final movement.
+				if( robotController.canMove( dir ) ) 
+				{
+					MapLocation moveTo = robotController.getLocation().add( dir );
+
+					if ( robotController.senseMine( moveTo ) == null || robotController.senseMine( moveTo ) == robotController.getTeam() )
+					{
+						robotController.move( dir );
+
+						robotController.setIndicatorString( 0, "Last direction moved: " + dir.toString() );									
+					}
+					else // Defusing code.
+					{
+						if ( enemies >= 1 || allies >= 6 )
+						{
+							robotController.defuseMine( moveTo );
+						}
+						else
+						{
+							robotController.yield(); // If there are too many enemies to defuse, do nothing.
+						}
+					}
+				}
+			}
+			if ( Math.random() < 0.01 && robotController.getTeamPower() > 5 ) 
+			{
+				// Write the number 5 to a position on the message board corresponding to the robot's ID
+				robotController.broadcast( robotController.getRobot().getID()%GameConstants.BROADCAST_MAX_CHANNELS, 5 );
+			}			
+		}		
+	}
+}
+
+
---
+++ Sensory.java
@@ -0,0 +1,111 @@
+package MasterPlayer;
+
+import battlecode.common.Direction;
+import battlecode.common.MapLocation;
+import battlecode.common.Robot;
+import battlecode.common.RobotController;
+import battlecode.common.Team;
+
+/**
+ * Attempt at separating functionality.
+ * 
+ * This contains all the global sensory functions for soldiers.
+ * 
+ * @author levonpowell
+ */
+public class Sensory 
+{
+	private static final int DEFAULT_ENEMY_DETECTION_RANGE = 15;
+	
+	/**
+	 * Returns the number of allied robots in the vicinity.
+	 * 
+	 * @param rc the RobotController
+	 * @param sightRange the robot's sight range
+	 * @return the number of enemies within a certain radius
+	 */
+	public static int getNumberOfLocalEnemies( RobotController rc, int sightRange )
+	{
+		return localTeamRobotCount( rc, sightRange, true );
+	}
+	
+	/**
+	 * Returns the number of allied robots in the vicinity.
+	 * 
+	 * @param rc the RobotController
+	 * @param sightRange the robot's sight range
+	 * @return the number of allies within a certain radius
+	 */
+	public static int getNumberOfLocalAllies( RobotController rc, int sightRange )
+	{
+		return localTeamRobotCount( rc, sightRange, false );
+	}
+	
+	/**
+	 * Returns the direction (relative to the robot) towards the enemy HQ
+	 * 
+	 * @param rc
+	 * @return 
+	 */
+	public static Direction getDirectionToEnemyHQ( RobotController rc )
+	{
+		return directionsToHQ( rc, true );
+	}
+	
+	public static Direction getDirectionToHQ( RobotController rc )
+	{
+		return directionsToHQ( rc, false );
+	}
+	
+	// End public methods.
+	// Being private methods
+	
+	/**
+	 * Returns the location of either the allied, or enemy HQ
+	 * 
+	 * @param rc
+	 * @param enemyTeam
+	 * @return 
+	 */
+	private static Direction directionsToHQ( RobotController rc, boolean enemyTeam )
+	{
+		MapLocation hqSquare;
+		
+		if ( enemyTeam )
+		{
+			hqSquare = rc.senseEnemyHQLocation();
+		}
+		else
+		{
+			hqSquare = rc.senseHQLocation();
+		}
+		
+		return rc.getLocation().directionTo( hqSquare );
+	}
+	
+	/**
+	 * This does all the lifting when asking "how many allies/enemies are around me?"
+	 * 
+	 * Implements the NumberOfLocalEnemies/Allies methods.
+	 * 
+	 * @param rc
+	 * @param range an integer of the sight range of the robot
+	 * @param enemies a boolean asking whether you want to count enemies or allies.
+	 * @return
+	 */
+	private static int localTeamRobotCount( RobotController rc, int range, boolean enemies )
+	{
+		Robot[] robotArray;
+		
+		Team team = rc.getTeam();
+		
+		if ( enemies )
+		{
+			team = team.opponent();
+		}
+		
+		robotArray = rc.senseNearbyGameObjects( Robot.class, range, team );
+		
+		return robotArray.length;		
+	}
+}
